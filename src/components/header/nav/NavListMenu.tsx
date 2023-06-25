ea20c4900ab25146d459fe6577c122923634414b
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-06-25 20:10:19 +0300

    feat: Add and style the NavListMenu

---
+++ src/components/header/nav/NavListMenu.tsx
@@ -0,0 +1,53 @@
+import classes from "./NavListMenu.module.css";
+
+const NavListMenu = () => {
+  return (
+    <div className={classes["nav-menu-container"]}>
+      <div className={classes["nav-menu"]}>
+        <ul className={classes["nav-menu__list"]}>
+          <li>
+            <h4>Shoes</h4>
+            <ul>
+              <li>Everyday Sneakers</li>
+              <li>Active Shoes</li>
+              <li>Slip-Ons</li>
+              <li>Golf Shoes</li>
+              <li>Hiking Shoes</li>
+              <li>Sandals</li>
+              <li>Water-Repellent Sneakers</li>
+              <li>High Tops</li>
+              <li>Sale</li>
+              <li>Shop all</li>
+            </ul>
+          </li>
+          <li>
+            <h4>Best Sellers</h4>
+            <ul>
+              <li>Tree Runner</li>
+              <li>Tree Dasher 2</li>
+              <li>Wool Runner</li>
+              <li>Trail Runner SWT</li>
+              <li>Tree Lounger</li>
+              <li>Tree Dasher Relay</li>
+              <li>Shop all</li>
+            </ul>
+          </li>
+          <li>
+            <h4>Featured</h4>
+            <ul className={classes["nav-menu__list__featured"]}>
+              <li>
+                <p>Tree Flyer, now on sale</p>
+              </li>
+              <li>
+                <p>Summer Styles</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div>&nbsp;</div>
+    </div>
+  );
+};
+
+export default NavListMenu;
--- src/components/header/nav/Navigation.tsx
+++ src/components/header/nav/Navigation.tsx
@@ -1,24 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 import MainNavList from "./MainNavList";
 import SecondaryNavList from "./SecondaryNavList";
-import Link from "next/link";
+import NavListMenu from "./NavListMenu";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
-    <nav className={classes.nav}>
-      <MainNavList />
-      <Link href="/" className={classes.logo}>
-        <Image
-          alt="All birds Logo"
-          src="//cdn.allbirds.com/image/upload/v1571355713/icons/allbirds-logo.svg"
-          width={120}
-          height={38.14}
-        />
-      </Link>
-      <SecondaryNavList />
-    </nav>
+    <div>
+      <nav className={classes.nav}>
+        <MainNavList />
+        <Link href="/" className={classes.logo}>
+          <Image
+            alt="All birds Logo"
+            src="//cdn.allbirds.com/image/upload/v1571355713/icons/allbirds-logo.svg"
+            width={120}
+            height={38.14}
+          />
+        </Link>
+        <SecondaryNavList />
+      </nav>
+      <NavListMenu />
+    </div>
   );
 };
 
