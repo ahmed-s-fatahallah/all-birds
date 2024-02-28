32cca9308dee0022a2758959ef584129cca0536e
Ahmed Saeed <115148623+ahmed-s-fatahallah@users.noreply.github.com>
2024-02-28 22:11:35 +0200

    feat: Add product approach component to the account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,11 @@
+import ProductApproach from "@/components/productApproach/ProductApproach";
 import ProfileComponent from "@/components/profileComponent/ProfileComponent";
 
 export default function Account() {
-  return <ProfileComponent />;
+  return (
+    <>
+      <ProfileComponent />;
+      <ProductApproach />
+    </>
+  );
 }
